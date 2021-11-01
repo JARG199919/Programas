97bb07491bae08322af8071ca6810885d9dc542f
jesus <00370321@uca.edu.sv>
2021-11-01 16:57:30 -0600

    Mejora de diseno de la tabla y arreglos de errores

--- main.c
+++ main.c
@@ -4,65 +4,102 @@
 #include <stdbool.h>
 #include <string.h>
 
+//costantes definidas en ingles
 #define DEFAULT_ROWS 9
 #define DEFAULT_COLUMNS 9
-
+#define COLOR_STRING_SIZE 12
+//Estructuras definidas
 struct size {
     int n;
     int m;
-    };
+};
 struct board {
     struct size dimensions;
-    char *content[]; 
-    };
-
+    char borderColor[COLOR_STRING_SIZE];
+    char contentColor[COLOR_STRING_SIZE]; 
+    char **content; 
+};
+//Declaracion de funciones
 struct size getBoardSize(int, const char *[]);
 void initBoard(struct board*);
+void freeBoard(struct board*);
 void displayBoard(struct board*);
 
 int main(int argc, const char *argv[]) {
-    srand(time(NULL));
     struct board boardMemory;
     boardMemory.dimensions = getBoardSize(argc, argv);
+    strcpy(boardMemory.borderColor, "\033[0;35m");
+    strcpy(boardMemory.contentColor, "\033[1;36m");
+    printf("Iniciando juego de memoria con tablero %d x %d\n",
+        boardMemory.dimensions.n, boardMemory.dimensions.m);
     initBoard(&boardMemory);
     displayBoard(&boardMemory);
+    freeBoard(&boardMemory);
     return 0;
 }
-
+//Estructura de la tabla 
 struct size getBoardSize(int argc, const char *argv[]) {
     struct size board = {DEFAULT_ROWS, DEFAULT_COLUMNS};
     if (argc >= 3) {
         int n = atoi(argv[1]);
         int m = atoi(argv[2]);
-
         board.n = (n == 0) ? DEFAULT_ROWS : n;
         board.m = (m == 0) ? DEFAULT_COLUMNS : m;
-
-        if ((board.n * board.m) % 2 != 0) {
-            printf("Seleccione una dimension de tablero que contenga una cantidad " "par de elementos\n");
-            exit(1);
-        }
     }
     return board;
 }
-/**
- * Llenar el contendio del tablero con parajes
- * posicionadas de forma aleatoria segun las dimensiones 
- * del tablero
- **/
-void initBoard(struct board* board) {
+//llenado de la tabla con valores aleatorios
+void initBoard(struct board *board) {
     int cRows = board->dimensions.n;
     int cColumns = board->dimensions.m;
-    printf("Llenando el tablero %d x %d", cRows, cColumns);
-}
-void displayBoard(struct board *board) {
-    printf("\n\n");
-    srand(time(NULL));
-    for (int i = 0 ; i < board->dimensions.n ; i ++) {
-        for( int j = 0 ; j < board->dimensions.m; j ++) {
-            printf("| %d ",rand()% (10-1)+1);
+    board->content = (char **) malloc(sizeof(char *) * cRows);
+    if (board->content != NULL) {
+        for(int i = 0; i < cRows; i++) {
+            (board->content)[i] = (char *) malloc(sizeof(char) * cColumns );
+            if ((board->content)[i] == NULL) {
+                printf("No se puede reservar memoria para el tablero");
+                exit(1);
+            }
+            for(int j = 0; j< cColumns; j++) {
+                board->content[i][j] = rand()% (10-1)+1;
+            }
         }
-        printf("|\n");
+    } 
+    else {
+        printf("No se puede reservar memoria para el tablero");
+        exit(1);
     }
-    printf("\n\n");
+}
+void freeBoard(struct board *board) {
+    int cRows = board->dimensions.n;
+    for(int i = 0 ;i < cRows; i++ ) {
+        free((board->content)[i]);
+    }
+    free(board->content);
+}
+//funcion de muestra en pantalla con decoracion
+void displayBoard(struct board *board) {
+    printf("\n\n%s┌", board->borderColor);
+    for (int j = 0; j < board->dimensions.m - 1; j++) {
+        printf("───┬");
+    }
+        printf("───┐\n");
+        for (int i = 0; i < board->dimensions.n - 1; i++) {
+            for (int j = 0; j < board->dimensions.m; j++) {
+            printf("│%s %d %s",board->contentColor, board->content[i][j], board->borderColor);
+        }
+        printf("│\n├");
+        for (int j = 0; j < board->dimensions.m - 1; j++) {
+            printf("───┼");
+        }
+        printf("───┤\n");
+        }
+        for (int j = 0; j < board->dimensions.m; j++) {
+        printf("│%s %d %s",board->contentColor, board->content[board->dimensions.n -1][j], board->borderColor);
+    }
+        printf("│\n└");
+        for (int j = 0; j < board->dimensions.m - 1; j++) {
+        printf("───┴");
+    }
+    printf("───┘\033[0m\n\n");
 }
