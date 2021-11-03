cba40498a7f8d32260c84c4b93d613b4cd9799b7
katara211 <92530269+katara211@users.noreply.github.com>
2021-11-03 09:01:01 -0600

    Update mainplayer.c

--- mainplayer.c
+++ mainplayer.c
@@ -1,1 +1,15 @@
-
+void innumeros(void){
+    int jugador1=0;
+    int jugador2=0;
+        printf("turno de jugador 1\n");
+         printf("ingrese el numero que quiere elegir: ");
+          scanf("%d",&jugador1);
+        printf("%d\n",jugador1);
+        
+        printf("turno de jugador 2\n");
+          printf("ingrese el numero que quiere elegir: ");
+            scanf("%d",&jugador2);
+        printf("%d\n",jugador2);
+    
+}
+void playerGame(void){
