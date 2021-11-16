92230584d6906d8127336ccf565d10c7193271d8
jesus <00370321@uca.edu.sv>
2021-11-16 14:33:24 -0600

    Juego terminado al 75% -Arreglar algunos errores y optinmizar el programa-

--- main.c
+++ main.c
@@ -4,9 +4,9 @@
 #include <stdbool.h>
 #include <string.h>
 
-//costantes definidas en ingles
-#define DEFAULT_ROWS 9
-#define DEFAULT_COLUMNS 9
+//costantes definidas en ES y hay algunas en ingles
+#define DEFAULT_ROWS 4
+#define DEFAULT_COLUMNS 4
 #define COLOR_STRING_SIZE 12
 
 //Estructuras definidas
@@ -24,59 +24,77 @@ struct board {
     struct size dimensions;
     char borderColor[COLOR_STRING_SIZE];
     char contentColor[COLOR_STRING_SIZE]; 
-    struct celda **content; 
+    struct celda **contenido; 
 };
 //Declaracion de funciones
 struct size getBoardSize(int, const char *[]);
 void initBoard(struct board*);
 void fillBoard(struct board*); 
-void freeBoard(struct board*);
+void liberarmemoria(struct board*);
 void displayBoard(struct board*);
-void displaycelda(char*, char*,struct celda);
-bool hasPairs(struct board*);
-void initGame(struct board *);
+void mostrar_celda(char*, char*,struct celda);
+bool formandopares(struct board*);
+void EstructuraDelJuego(struct board *);
+int memoryGame(struct board *);
+void reconfigGame(struct board *);
+void showGameMenu();
+void cleanBuffer();
 
 int main(int argc, const char *argv[]) {
+    FILE *pointFile;
     struct board boardMemory;
     boardMemory.dimensions = getBoardSize(argc, argv);
-    initGame(&boardMemory);
-    char cont = 'Y';
+    EstructuraDelJuego(&boardMemory);
+    system("clear");
+    printf("Bienvenido al juego de memoria\n\n\n");
+
+    int opcion;
+
     do {
-        int xPos1, yPos1, xPos2, yPos2;
-        displayBoard(&boardMemory);
-        printf("Seleccione la tarjeta que desea ver (-,#): ");
-        scanf("%d,%d",&xPos1,&yPos1);
-        xPos1--; 
-        yPos1--;
-        boardMemory.content[xPos1][yPos1].status = SHOW;
-        system("clear"); 
-        displayBoard(&boardMemory);
-        
-        printf("Seleccione la segunda tarjeta que desea ver (#,-): ");
-        scanf("%d,%d",&xPos2,&yPos2);
-        xPos2--;
-        yPos2--;
-        boardMemory.content[xPos2][yPos2].status = SHOW;
-        displayBoard(&boardMemory);
-        if (boardMemory.content[xPos2][yPos2].card == boardMemory.content[xPos1][yPos1].card) {
-             boardMemory.content[xPos2][yPos2].status = MATCH;
-             boardMemory.content[xPos1][yPos1].status = MATCH;
-             printf("Has encontrado una pareja");
-        } else {
-             printf("La seleccion no forma una pareja");
-             boardMemory.content[xPos2][yPos2].status = HIDDEN;
-             boardMemory.content[xPos1][yPos1].status = HIDDEN;
-        } 
-        scanf("%c",&cont);
-        scanf("%c",&cont);
-        system("clear");       
-    } while(hasPairs(&boardMemory));
-    freeBoard(&boardMemory);
+        int puntaje;
+        showGameMenu();
+        printf("\n: ");
+        scanf("%d", &opcion);
+        switch (opcion) {
+            case 1:
+                puntaje = memoryGame(&boardMemory);
+                char nombre[20];
+                printf("Ingresa tu nombre de jugador: ");
+                scanf("%s", nombre);
+                pointFile = fopen("./puntos.txt","a");
+                fprintf(pointFile,"%d,%s\n",puntaje,nombre);
+                fclose(pointFile);// Guardar en un archivo
+                break;
+            case 2:
+                system("clear");
+                int puntajeTop = 0;
+                char nombreJugador[20];
+                pointFile = fopen("./puntos.txt","r");
+                while(fscanf(pointFile,"%d,%s", &puntajeTop, nombreJugador)!= EOF) {
+                    printf("%s\t%d\n",nombreJugador,puntajeTop);
+                }
+                fclose(pointFile);
+                printf("\n\n");
+                break;
+            case 3:
+                reconfigGame(&boardMemory);
+                break;
+            case 4:
+                printf("Gracias por jugar\n");
+                liberarmemoria(&boardMemory);
+                break;
+            default:
+                printf("Obcion no encontrada, por favor ingrese una opcion valida\n");
+                break;
+        }
+    } 
+    while (opcion != 4);
     return 0;
 }
-void initGame(struct board *boardMemory) {
-    strcpy(boardMemory->borderColor, "\033[0;35m");
-    strcpy(boardMemory->contentColor, "\033[1;36m");
+//Forma y colores de la tabla
+void EstructuraDelJuego(struct board *boardMemory) {
+    strcpy(boardMemory->borderColor, "\033[0;35m");//color de los bordes
+    strcpy(boardMemory->contentColor, "\033[1;36m");//color de el contenido
     initBoard(boardMemory);
     fillBoard(boardMemory);
     system("clear"); 
@@ -98,19 +116,19 @@ struct size getBoardSize(int argc, const char *argv[]) {
 }
 //llenado de la tabla con valores aleatorios
 void initBoard(struct board *board) {
-    int cRows = board->dimensions.n;
-    int cColumns = board->dimensions.m;
-    board->content = (struct celda **) malloc(sizeof(struct celda*) * cRows);
-    if (board->content != NULL) {
-        for(int i = 0; i < cRows; i++) {
-            (board->content)[i] = (struct celda *) malloc(sizeof(struct celda) * cColumns );
-            if ((board->content)[i] == NULL) {
+    int nFilas = board->dimensions.n;
+    int nColumnas = board->dimensions.m;
+    board->contenido = (struct celda **) malloc(sizeof(struct celda*) * nFilas);
+    if (board->contenido != NULL) {
+        for(int i = 0; i < nFilas; i++) {
+            (board->contenido)[i] = (struct celda *) malloc(sizeof(struct celda) * nColumnas );
+            if ((board->contenido)[i] == NULL) {
                 printf("No se puede reservar memoria para el tablero");
                 exit(1);
             }
-            for(int j = 0; j< cColumns; j++) {
-                board->content[i][j].card = '\0';
-                board->content[i][j].status = HIDDEN;
+            for(int j = 0; j< nColumnas; j++) {
+                board->contenido[i][j].card = '\0';
+                board->contenido[i][j].status = HIDDEN;
             }
         }
     } 
@@ -119,43 +137,42 @@ void initBoard(struct board *board) {
         exit(1);
     }
 }
-
+//Llenado de tabla
 void fillBoard(struct board *board) {
-    int cRows = board->dimensions.n;
-    int cColumns = board->dimensions.m;
+    int nFilas = board->dimensions.n;
+    int nColumnas = board->dimensions.m;
     srand(time(NULL));
-    int nOptions = (cColumns * cRows) / 2;
+    int nOptions = (nColumnas * nFilas) / 2;
     char options[nOptions];
     for (int j = 0; j < nOptions; j++) {
-        options[j] = 'A' + rand() % 25;
+        options[j] = 'A' + rand() % 25;//Llenando con letras aleatorias //TODO: cambiar a numeros aleatorios del 1 al 9
     }
     int i = 0;
     while (i < nOptions) {
         int xPos1, yPos1, xPos2, yPos2;
         do {
-            xPos1 = rand() % cRows;
-            yPos1 = rand() % cColumns;
-        } while (board->content[xPos1][yPos1].card != '\0');
+            xPos1 = rand() % nFilas;
+            yPos1 = rand() % nColumnas;
+        } while (board->contenido[xPos1][yPos1].card != '\0');
         do {   
-            xPos2 = rand() % cRows;
-            yPos2 = rand() % cColumns;
-        } while (board->content[xPos2][yPos2].card != '\0');
+            xPos2 = rand() % nFilas;
+            yPos2 = rand() % nColumnas;
+        } while (board->contenido[xPos2][yPos2].card != '\0');
         if (xPos1 == xPos2 && yPos1 == yPos2) {
             continue;
         }
-        // printf("Llenando posion %d %d y %d %d %c\n", xPos1,yPos1, xPos2,yPos2, options[i]);
-        board->content[xPos2][yPos2].card = options[i];
-        board->content[xPos1][yPos1].card = options[i];
+        board->contenido[xPos2][yPos2].card = options[i];
+        board->contenido[xPos1][yPos1].card = options[i];
         i++;
     }
 }
-
-void freeBoard(struct board *board) {
-    int cRows = board->dimensions.n;
-    for(int i = 0 ;i < cRows; i++ ) {
-        free((board->content)[i]);
+//liberacion de memoria
+void liberarmemoria(struct board *board) {
+    int nFilas = board->dimensions.n;
+    for(int i = 0 ;i < nFilas; i++ ) {
+        free((board->contenido)[i]);
     }  
-    free(board->content);
+    free(board->contenido);
 }
 //funcion de muestra en pantalla con decoracion
 void displayBoard(struct board *board) {
@@ -168,7 +185,7 @@ void displayBoard(struct board *board) {
             for (int j = 0; j < board->dimensions.m; j++) {
             char *contentColor = board->contentColor;
             char *borderColor = board->borderColor;
-            displayCelda(borderColor, contentColor, board->content[i][j]);
+            mostrar_celda(borderColor, contentColor, board->contenido[i][j]);
         }
         printf("│\n├");
         for (int j = 0; j < board->dimensions.m - 1; j++) {        
@@ -179,7 +196,7 @@ void displayBoard(struct board *board) {
         for (int j = 0; j < board->dimensions.m; j++) {
         char *contentColor = board->contentColor;
         char *borderColor = board->borderColor;
-        displayCelda(borderColor, contentColor,board->content[board->dimensions.n - 1][j]);
+        mostrar_celda(borderColor, contentColor,board->contenido[board->dimensions.n - 1][j]);
         }
         printf("│\n└");
         for (int j = 0; j < board->dimensions.m - 1; j++) {
@@ -187,38 +204,118 @@ void displayBoard(struct board *board) {
     }
     printf("───┘\033[0m\n\n");
 }
-
-void displayCelda(char *borderColor, char *contentColor, struct celda celda) {
-    char *hiddenSymbol = "♠"; 
-    char content[2];
-    content[0] = celda.card;
-    content[1] = '\0';
-    char *matchString = " "; 
-
+//mostrando o ocultando variables
+void mostrar_celda(char *borderColor, char *contentColor, struct celda celda) {
+    char *ocultarvariable = "♠"; 
+    char contenido[2];
+    contenido[0] = celda.card;
+    contenido[1] = '\0';
+    char *vacio = " "; 
     char *displayString; 
+
+    //Modos de presentacion de contenido
     switch (celda.status){
         case HIDDEN:
-            displayString = hiddenSymbol;
+            displayString = ocultarvariable;
             break;
         case SHOW:
-            displayString = content;
+            displayString = contenido;
             break;
         case MATCH:
-            displayString =  matchString;
+            displayString =  vacio;
             break;
     }
     printf("│%s %s %s", contentColor, displayString, borderColor);
 }
-bool hasPairs(struct board *board) {
-    int cRow = board->dimensions.n;
-    int cColumns = board->dimensions.m;
+//Creacion de pares
+bool formandopares(struct board *board) {
+    int nFilas = board->dimensions.n;
+    int nColumnas = board->dimensions.m;
 
-    for (int i = 0; i < cRow; i++) {
-        for (int j = 0; j < cColumns; j++) {
-            if (board->content[i][j].status == HIDDEN) {
+    for (int i = 0; i < nFilas; i++) {
+        for (int j = 0; j < nColumnas; j++) {
+            if (board->contenido[i][j].status == HIDDEN) {
                 return true;
             }
         }
     }
     return false;
+}
+void cleanBuffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
+int memoryGame(struct board *boardMemory) {
+    char cont;
+    int puntajes = 0;
+    do {
+        int xPos1, yPos1, xPos2, yPos2;
+        printf("\nPuntos %d", puntajes);
+        displayBoard(boardMemory); 
+
+        printf("Seleccione la tarjeta que desea ver (#,#): ");
+        scanf("%d,%d", &xPos1, &yPos1);
+        xPos1--;
+        yPos1--;
+        system("clear");
+
+        boardMemory->contenido[xPos1][yPos1].status = SHOW;
+
+        printf("\nPuntos %d", puntajes);
+        displayBoard(boardMemory);
+
+        printf("Seleccione la segunda tarjeta que desea ver (#,#): ");
+        scanf("%d,%d", &xPos2, &yPos2);
+        xPos2--;
+        yPos2--;
+        system("clear");
+
+        boardMemory->contenido[xPos2][yPos2].status = SHOW;
+        printf("\nPuntos %d", puntajes);
+        displayBoard(boardMemory);
+        scanf("%c", &cont);
+
+        if (boardMemory->contenido[xPos2][yPos2].card ==
+            boardMemory->contenido[xPos1][yPos1].card) {
+            boardMemory->contenido[xPos2][yPos2].status = MATCH;
+            boardMemory->contenido[xPos1][yPos1].status = MATCH;
+            printf("\nHas encontrado una pareja\n");
+            puntajes++;
+        } else {
+            printf("\nLa seleccion no forma una pareja\n");
+            boardMemory->contenido[xPos2][yPos2].status = HIDDEN;
+            boardMemory->contenido[xPos1][yPos1].status = HIDDEN;
+        }
+        printf("Presione cualquier tecla para continuar o N para salir: ");
+        scanf("%c", &cont);
+        system("clear");
+        if (cont == 'N' || cont == 'n') {
+            return puntajes;
+        }
+        cleanBuffer();
+    } 
+    while (formandopares(boardMemory));
+    return puntajes;
+}
+void reconfigGame(struct board *boardMemory) {
+    int n = 1, m = 1;
+
+    if ((n * m) % 2 != 0) {
+        printf("Ingrese la cantidad de filas: ");
+        scanf("%d", &n);
+        printf("Ingrese la cantidad de columnas: ");
+        scanf("%d", &m);
+    }
+
+    liberarmemoria(boardMemory);
+
+    boardMemory->dimensions.n = n;
+    boardMemory->dimensions.m = m;
+
+    EstructuraDelJuego(boardMemory);
+
+    system("clear");
+}
+void showGameMenu() {
+    printf("1.Jugar\n2.Ver Puntajes\n3.Configuración\n4.Salir");
 }
